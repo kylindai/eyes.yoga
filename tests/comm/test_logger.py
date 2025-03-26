14219342f0fbc3506db94847409750551a125575
kylindai <kylin@hotmail.com>
2025-03-26 08:15:21 +0800

    for test

--- tests/comm/test_logger.py
+++ tests/comm/test_logger.py
@@ -13,7 +13,7 @@ class LoggerTest(BaseTestCase):
     def test_logger(self):
         LOG_CARE("hello world")
 
-        logger = Logger(self, log_level=LogLevel.TRACE, log_file_name='test.log')
+        logger = Logger(self, log_level=LogLevel.INFO, log_file_name='test.log')
         logger.error("test ...")
         logger.info("this is a normal info.")
         logger.debug("this is a debug info.")
